10244ec8e222b6a7488752cb06abf32f7c860db4
uyamini <uyamini1996@gmail.com>
2024-03-22 02:25:46 -0500

    added cached elements

--- js/script.js
+++ js/script.js
@@ -20,6 +20,9 @@ let currentPlayer;
 let gameActive;
 
 /*----- cached elements  -----*/
+const chessboardEl = document.getElementById('chessboard');
+const messageEl = document.getElementById('message');
+const resetBtn = document.getElementById('reset');
 
 /*----- event listeners -----*/
 
