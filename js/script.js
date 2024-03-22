6eac74f5fb875e57322213a69d558a524e096646
uyamini <uyamini1996@gmail.com>
2024-03-22 02:24:21 -0500

    added js blueprint

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*----- constants -----*/
+const BOARD_SIZE = 8;
+const PIECES = {
+  EMPTY: ' ',
+  PAWN: 'P',
+  ROOK: 'R',
+  KNIGHT: 'N',
+  BISHOP: 'B',
+  QUEEN: 'Q',
+  KING: 'K',
+};
+const PLAYERS = {
+  WHITE: 'W',
+  BLACK: 'B',
+};
+
+/*----- state variables -----*/
+
+/*----- cached elements  -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
